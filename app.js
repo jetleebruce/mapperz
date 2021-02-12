61d43f80cee264d7e003696e0ddf46d68c285779
Serge Modin <modinsv@yandex.ru>
2021-02-12 20:20:12 +0300

    Creating basic Express App

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-const express = require
+const express = require('express');
+const app = express();
+const path = require('path')
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.get('/', (req,res) =>{
+    res.render('home')
+})
+
+app.listen(3000, (req,res) =>{ 
+   console.log("Serving on port 3000")
+})
