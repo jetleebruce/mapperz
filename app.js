f137b61a75d395dddbd3abb127e4a4cfe508aed0
Serge Modin <modinsv@yandex.ru>
2021-02-13 11:08:22 +0300

    POST a place to the Database

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const path = require('path')
+const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 
@@ -7,13 +7,13 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     useCreateIndex:true,
     useUnifiedTopology:true
-})
+});
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("DB Connected")
-})
+});
 
 const app = express();
 
@@ -21,20 +21,33 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req,res) =>{
     res.render('home')
-})
+});
 
 app.get('/campgrounds', async (req,res) =>{
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', {campgrounds})
+});
+
+app.get('/campgrounds/new', async (req,res) => {
+    res.render('campgrounds/new')
+})
+
+app.post('/campgrounds', async(req,res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
 })
 
 app.get('/campgrounds/:id', async (req,res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show', {campground})
-})
+});
+
+
 
 app.listen(3000, (req,res) =>{ 
    console.log("Serving on port 3000")
