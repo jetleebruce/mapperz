e8555ab61ad3303fa687d6f51f5a7a1fb370b172
Serge Modin <modinsv@yandex.ru>
2021-02-13 10:33:49 +0300

    Showing the list of places and a single place

--- app.js
+++ app.js
@@ -25,10 +25,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.get('/', (req,res) =>{
     res.render('home')
 })
-app.get('/makecampground', async (req,res) =>{
-   const camp = new Campground({title: "My backyard", description: "Cheap place"});
-   await camp.save()
-   res.send(camp)
+
+app.get('/campgrounds', async (req,res) =>{
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', {campgrounds})
+})
+
+app.get('/campgrounds/:id', async (req,res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/show', {campground})
 })
 
 app.listen(3000, (req,res) =>{ 
