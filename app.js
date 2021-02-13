d328b731ab21d27326a363f3da2ca0014754428e
Serge Modin <modinsv@yandex.ru>
2021-02-13 20:53:20 +0300

    Added ejs-mate and styled pages, added bootstarp cdn

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate')
+const morgan = require('morgan')
 const methodOverride = require('method-override')
 const Campground = require('./models/campground');
 
@@ -18,12 +20,13 @@ db.once("open", () => {
 
 const app = express();
 
-
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
+app.use(morgan('tiny'))
 
 app.get('/', (req,res) =>{
     res.render('home')
